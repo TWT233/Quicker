1a18d2a581fc2cfd12ce6b7fef76ebe2b32630a1
wangqiyilang <wangqiyilang@bytedance.com>
2026-03-27 13:01:23 +0800

    Revert "damp"
    This reverts commit e28f87bcc24945252c3c6127f927ed985bd719ec.

--- Patch/Speed.cs
+++ Patch/Speed.cs
@@ -1,7 +1,5 @@
-﻿using System.Reflection;
-using Godot;
+﻿using Godot;
 using HarmonyLib;
-using MegaCrit.Sts2.Core.Helpers;
 using MegaCrit.Sts2.Core.Nodes;
 using MegaCrit.Sts2.Core.Nodes.Vfx.Utilities;
 using Quicker.Util;
@@ -26,74 +24,5 @@ public class Speed
     private static void NGameReady(NGame __instance)
     {
         if (Context.IsDeltaMultiplied) Engine.TimeScale = Context.DeltaMultiplier;
-    }
-}
-
-[HarmonyPatch]
-public static class SmoothDampFloatPatch
-{
-    public static MethodBase TargetMethod()
-    {
-        return typeof(MathHelper).GetMethod(
-            nameof(MathHelper.SmoothDamp),
-            BindingFlags.Public | BindingFlags.Static,
-            null,
-            [typeof(float), typeof(float), typeof(float).MakeByRefType(), typeof(float), typeof(float), typeof(float)],
-            null
-        )!;
-    }
-
-    [HarmonyPrefix]
-    public static bool Prefix(
-        float current,
-        float target,
-        ref float currentVelocity,
-        float smoothTime,
-        float deltaTime,
-        float maxSpeed,
-        ref float __result)
-    {
-        if (!Context.IsDeltaMultiplied || Context.DeltaMultiplier <= 1.0f) return true;
-        __result = target;
-        currentVelocity = 0;
-        return false;
-    }
-}
-
-[HarmonyPatch]
-public static class SmoothDampVector2Patch
-{
-    public static MethodBase TargetMethod()
-    {
-        return typeof(MathHelper).GetMethod(
-            nameof(MathHelper.SmoothDamp),
-            BindingFlags.Public | BindingFlags.Static,
-            null,
-            [
-                typeof(Vector2),
-                typeof(Vector2),
-                typeof(Vector2).MakeByRefType(),
-                typeof(float),
-                typeof(float),
-                typeof(float)
-            ],
-            null
-        )!;
-    }
-
-    [HarmonyPrefix]
-    public static bool Prefix(
-        Vector2 current,
-        Vector2 target,
-        ref Vector2 currentVelocity,
-        float smoothTime,
-        float deltaTime,
-        float maxSpeed,
-        ref Vector2 __result)
-    {
-        if (!Context.IsDeltaMultiplied || Context.DeltaMultiplier <= 1.0f) return true;
-        __result = target;
-        currentVelocity = Vector2.Zero;
-        return false;
     }
 }
